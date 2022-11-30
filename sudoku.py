bf1ea339ef46f538384eec973bca3023876e3faa
Jaedon Taylor <jaedonataylor@gmail.com>
2022-11-30 15:52:47 -0500

    add

--- sudoku.py
+++ sudoku.py
@@ -1,2 +1,14 @@
 from sudoku_generator import SudokuGenerator
 import pygame
+
+pygame.init()
+
+pygame.display.set_caption("Sudoko")
+
+screen = pygame.display.set_mode((350, 350))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
