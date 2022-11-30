f0489984799e695b48372181e235bcf2193a2f87
Jaedon Taylor <jaedonataylor@gmail.com>
2022-11-30 17:25:23 -0500

    j

--- sudoku.py
+++ sudoku.py
@@ -8,6 +8,7 @@ pygame.display.set_caption("Sudoko")
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+user_font = pygame.font.Font(None, USERADD_FONT)
 def draw_game():
     #Draw defining borders
     for i in range(1, BOARD_ROWS):
@@ -19,11 +20,14 @@ def draw_game():
             pygame.draw.line(screen, LINE_COLOR, ((j * i * SQUARE_SIZE)/3,0), ((j * i * SQUARE_SIZE)/3, WIDTH), 2)
         pygame.draw.line(screen, LINE_COLOR, (i * SQUARE_SIZE, 0), (i * SQUARE_SIZE, WIDTH), LINE_WIDTH)
 
-
+def sketch(value):
+    number_surf = user_font.render(str(value), 0, USERADD_COLOR)
+    number_rect = number_surf.get_rect(center= (300, 300))
+    screen.blit(number_surf, number_rect)
 
 screen.fill(BG_COLOR)
 draw_game()
-
+sketch(5)
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
