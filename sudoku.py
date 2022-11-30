c7694308370030b0d00b8fd52fd65f6a3d25ff2e
Jaedon Taylor <jaedonataylor@gmail.com>
2022-11-30 16:29:23 -0500

    add

--- sudoku.py
+++ sudoku.py
@@ -1,11 +1,18 @@
 from sudoku_generator import SudokuGenerator
+from constants import *
 import pygame
 
 pygame.init()
 
 pygame.display.set_caption("Sudoko")
 
-screen = pygame.display.set_mode((350, 350))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def draw_game():
+    pygame.draw.line(screen, LINE_COLOR, (0, SQUARE_SIZE),(WIDTH, SQUARE_SIZE), LINE_WIDTH)
+
+screen.fill(BG_COLOR)
+draw_game()
 
 while True:
     for event in pygame.event.get():
@@ -13,4 +20,3 @@ while True:
             pygame.quit()
     pygame.display.update()
 
-#OK
